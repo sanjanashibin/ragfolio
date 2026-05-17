f8ddd750d921bfa10d1b77eab329e66726980291
sanjanashibin <sanjanaelizabeths@gmail.com>
2026-05-17 17:02:59 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
+set -o errexit
 
-# install python deps
 pip install -r requirements.txt
 
-# build frontend
 cd frontend
 npm install
 npm run build
 cd ..
 
-# create embeddings
-python rag/create-embeddings.py
+uv run rag/create-embeddings.py
